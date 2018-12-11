441a754473a62efb07924273499a98a112b8bd8f
David_L <45248865+lvchengshang@users.noreply.github.com>
2018-12-11 23:27:30 +0800

    Add files via upload

---
+++ config/base.php
@@ -0,0 +1,105 @@
+<?php return array (
+  'site_name' => 'LVCS-OneIndex',
+  'password' => 'lvcs',
+  'style' => 'nexmoe',
+  'onedrive_root' => '/',
+  'cache_type' => 'secache',
+  'cache_expire_time' => 3600,
+  'cache_refresh_time' => 600,
+  'root_path' => '?',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video' => 
+    array (
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => true,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'client_secret' => 'rbBSPKO143;|hhzubFL91*}',
+  'client_id' => 'd97d6e81-d835-4c53-a81a-c253361b5b11',
+  'redirect_uri' => 'https://ju.tn/',
+  'refresh_token' => 'OAQABAAAAAAC5una0EUFgTIF8ElaxtWjTWFEWzO6IvoGwUz04VR3zsETwGe3Vy9B3K78az8kk8iybYIWnkxfKMM1i_FYRqXh9zwo2taQmJtf3Shqa3syXr_TzUnlNsgAyh-DUkR3yJmYKhh2rdSS0PpkED8ZXXZrAQX3nsXnFNOmKJ9Sk68rK3H8IxrQsu0JQ6IwzAu3HIyrYua8eXLtdgDfkUkVNja3n0Tp5sVVxvXeUJJgetpbZA34FfV8JI0MVSdcl0drCpCyJHieNa3-4jdP3PLT_byo59OCus2lYaxajIYKvwZpME2NRLazaxkRSlozPmAMppw1OVeD_82a5S3iKYE9i7eZ3yPHPjT3F2KhcdGBm2-vmtv3EbaqFWXL5zQz35vIA7ZT0zjqMGX6MmlSKc2fLb1TDye4e1iAFoL_IVRDYTOO8TEbbR36DiY68w8z9P2E8Sr4KjqYDOz9MK8_x-YNt33FMNSmGZebaTobdH2l1xEj7ZnbzO6oz4_Jgq-OVaItPdecQjX8EULI1DtgP7XvWyx1t-e-kZAgAxWUGnAoPU317HdjIk5lEWoRYak7wCwnmZ6Nn3A3QUDao3ClBYUDjrIF-E6K8Cit2nkdSOSqQiYRoSoywkPT8DwOJtfd3Py6FHrFrj4VI4wmO8XnUSG1DUGwsY8aWDIkNwdTP1pSfAD4HhiyZgFyju8jb5jbkuLbZrG9RCsOVV-hUJVsaHOmCt5c1ffDnb6tGZ0PPVeGAzL2GcJh46uW1pDCue78y01sxe7GR8gJ-pMUnorE5XPsZR9_tA1BSHdQW5xFn7bimYwtb5CAA',
+  'onedrive_hide' => '',
+);
